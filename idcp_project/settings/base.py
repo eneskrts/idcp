b8c154259efe2cd81dd6bb837b7460c9c33eb0b1
Enes <enes.karatas@akinon.com>
2022-11-16 08:39:50 +0300

    rest framework added

--- idcp_project/settings/base.py
+++ idcp_project/settings/base.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 3rd party
+    'rest_framework',
     # our apps
     'authentication',
     'appointment',
