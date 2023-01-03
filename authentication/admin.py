e8aec8c4cfe91cda1a929b16f7024fc79540a266
HKalhan <hasret.kalhann@gmail.com>
2023-01-03 18:28:55 +0300

    User model has been updated. Register, login process completed.

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
+from authentication.models import Country,City,User,Profession,Education,Experience,Profile
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(User)
+admin.site.register(Profession)
+admin.site.register(Education)
+admin.site.register(Experience)
+admin.site.register(Profile)
