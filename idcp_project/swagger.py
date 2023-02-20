98f8215914192756eeb02339872de0434a0f32a6
anilcelik0 <anilcelik046@gmail.com>
2023-02-20 14:49:47 +0300

    fixed swager title

--- idcp_project/swagger.py
+++ idcp_project/swagger.py
@@ -6,7 +6,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="MEBRE API",
+        title="IDCP API",
         default_version='v1',
         description="MEBRE API DESCRIPTION",
         terms_of_service="https://www.ourapp.com/policies/terms/",
