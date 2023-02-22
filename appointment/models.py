2971217a7cb1f2a069638986439538a00507d448
anilcelik0 <anilcelik046@gmail.com>
2023-02-22 14:42:22 +0300

    fixed model problems

--- appointment/models.py
+++ appointment/models.py
@@ -33,13 +33,12 @@ class MeetingRoom(BaseModel):
     meeting_name = models.CharField(max_length=200, null=False)
     meeting_description = models.CharField(max_length=1000)
 
-    attendees = models.ManyToManyField(to='auth.user', related_name="attendees", through="AppointmentRequest")
+    attendees = models.ManyToManyField(get_user_model(), related_name="attendees", through="AppointmentRequest")
 
     meeting_url = models.CharField(max_length=1000)
     meeting_start_time = models.DateTimeField(null=False)
 
-    meeting_status = models.CharField(choices=StatusOption.choices, default=StatusOption.ACTIVE, null=False,
-                                      max_length=20)
+    meeting_status = models.CharField(choices=StatusOption.choices, default=StatusOption.ACTIVE, max_length=20)
 
     def __str__(self):
         return self.meeting_name
@@ -49,8 +48,7 @@ class AppointmentRequest(BaseModel):
     meeting = models.ForeignKey("appointment.MeetingRoom", on_delete=models.CASCADE, related_name="meeting_room")
     doctor = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="doctor")
 
-    doctor_request = models.CharField(choices=DoctorsRequest.choices, default=DoctorsRequest.WAITING, null=False,
-                                      max_length=20)
+    doctor_request = models.CharField(choices=DoctorsRequest.choices, default=DoctorsRequest.WAITING,max_length=20)
     request_status = models.CharField(choices=StatusOption.choices, default=StatusOption.ACTIVE, max_length=20)
     explanation = models.CharField(max_length=500)
 
