835f65583aeb1390ede67977f77fc8f27227cd55
anilcelik0 <anilcelik046@gmail.com>
2023-03-01 09:20:47 +0300

    added country filterset

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,33 @@ from rest_framework import viewsets
 from authentication.models import City,Country,User,Experience,Education,Profile,Profession
 from authentication.serializers import UpdateUserSerializer,CitySerializer,CountrySerializer,UserSerializer,ExperienceSerializer,EducationSerializer,ProfileSerializer,ProfessionSerializer
 
+# Filterset
+from django_filters import FilterSet, DateTimeFromToRangeFilter, rest_framework as filters
+from django_filters.widgets import RangeWidget
+
+
+class UserFilter(FilterSet):
+    country = filters.CharFilter(label="Country")
+    class Meta:
+        model = User
+        fields = ["username","country"]
+        
+        
+    def filter_queryset(self, queryset):
+        if self.data:
+            if self.data["country"]: 
+                country = self.data["country"]
+                
+                queryset = queryset.filter(profile__city__country__name=country)
+            
+            return queryset
+        
+        return queryset
+    
+
+
+
+
 class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.all()
     serializer_class = CitySerializer
@@ -13,6 +40,8 @@ class CountryViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = UserFilter
  
 class ExperienceViewSet(viewsets.ModelViewSet):
     queryset = Experience.objects.all()
